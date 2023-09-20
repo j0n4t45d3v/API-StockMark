5f219103d43aa1ca3626d4fad13c7d94a8e7dfcb
Jonatas <delimajonatas875@gmail.com>
2023-09-20 09:05:32 -0300

    test: teste do service task

--- microservices/task-service/__test__/testando-jest.spec.js
+++
@@ -1,9 +0,0 @@
-function sum(x, y) {
-	return x + y;
-}
-
-describe('testando', () => {
-	test('should sum values' , () => {
-		expect(sum(1, 2)).toEquals(3);
-	})
-})
--- microservices/task-service/src/service/task.service.js
+++ microservices/task-service/src/service/task.service.js
@@ -2,7 +2,10 @@
 import { Task } from "../model/task.schema.js";
 
 async function createTask(req){
-	const {} = req;
-	const task = new Task({});
-	task.save();
+	//const {} = req;
+	//const task = new Task({});
+	//task.save();
+	return "Tarefa criada!"
 }
+
+export {createTask}
